b3f155518fbb4e401290a89555cf7ed49b0205a3
Edwin Ramirez Ventura <edwin9870@gmail.com>
2017-06-06 16:44:44 -0400

    Init project

---
+++ jokeprovider/src/main/java/com/edwin/JokeProducer.java
@@ -0,0 +1,12 @@
+package com.edwin;
+
+/**
+ * Created by Edwin Ramirez Ventura on 6/6/2017.
+ */
+
+public class JokeProducer {
+
+    public String getJoke() {
+        return "Don't break anybody's heart; they only have 1. Break their bones; they have 206.";
+    }
+}
